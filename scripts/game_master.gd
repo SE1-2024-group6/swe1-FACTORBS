bdb29b06a1f2a6b1a24da763736e4a1a13102095
LlamaByDay <nateg333552@gmail.com>
2024-12-02 05:09:10 -0500

    Number Generation
    Added snake number generation, added cannon number generation, made orbs clearable, made snakes clearable, made new snakes spawn when snakes are cleared, made score update when orbs are cleared, made snake speed dynamic, made snakes spawn on a timer, created and then fixed a lot of bugs
    
    Game is now officially playable, potentially even fun

--- scripts/game_master.gd
+++ scripts/game_master.gd
@@ -1,27 +1,79 @@
 extends Node2D
 
 @onready var score_label = $"Score"
+@onready var SnakeTimer = $"SnakeTimer"
 
 var current_score = 0
-var previous_score = -1
+# var previous_score = -1
 var Snake = load("res://scenes/snake.tscn")
+var Terrarium = []
+var ValidNumbers = []
+var Random = RandomNumberGenerator.new()
+var TimerLength = 23
 
 func SpawnSnake() -> void:
+	if (!SnakeTimer.is_stopped()):
+		SnakeTimer.stop()
+	SnakeTimer.start(TimerLength)
 	var NewSnake = Snake.instantiate()
 	add_child(NewSnake)
+	Terrarium.append(NewSnake)
 	NewSnake.Generate(GenerateNumbers())
+	
 
 func GenerateNumbers():
-	return [1,2,3,4]
+	var SnakeNumbers = []
+	for i in range(5): # make variable when difficulty is implemented
+		SnakeNumbers.append(ValidNumbers[Random.randi_range(0, 25)]) #also make difficulty variable
+	return SnakeNumbers
+
+func GetRandomSnorb():
+	var NumSnakes = len(Terrarium)
+	var SnakeSelect
+	if NumSnakes == 1:
+		SnakeSelect = Terrarium[0]
+	elif NumSnakes > 1:
+		SnakeSelect = Terrarium[Random.randi_range(1, NumSnakes)-1]
+	else:
+		return -1
+	return SnakeSelect.GetOrb(Random.randi_range(1, SnakeSelect.GetLength())-1)
+
+func DeleteSnake(DeadSnake):
+	for i in range(0, len(Terrarium)):
+		print(len(Terrarium))
+		if Terrarium[i] == DeadSnake:
+			Terrarium.remove_at(i)
+			DeadSnake.queue_free()
+			if len(Terrarium) == 0:
+				call_deferred("SpawnSnake") # Don't ask
+			return
+		else:
+			print("Failed to kill snake")
 
 func _ready() -> void:
+	Random.randomize()
+	score_label.text = " SCORE: 0" 
+	var Primes = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97]
+	for i in range(2,100):
+		if not i in Primes:
+			ValidNumbers.append(i) # This is an awful idea, if anyone has a better one please implement it
 	SpawnSnake()
 
-func _process(_delta: float) -> void:
-	update_score()
+#func _process(_delta: float) -> void:
+#	update_score()
 
 # function to update score only when it changes (instead of every frame)
-func update_score() -> void:
-	if current_score != previous_score:
-		score_label.text = " SCORE: " + str(current_score)
-		previous_score = current_score
+# 
+#		Nate here, this is kind of pointless so I replaced it
+#
+#func update_score() -> void:
+#	if current_score != previous_score:
+#		score_label.text = " SCORE: " + str(current_score)
+#		previous_score = current_score
+
+func UpdateScore():
+	current_score += 1
+	score_label.text = " SCORE: " + str(current_score)
+
+func _on_snake_timer_timeout() -> void:
+	call_deferred("SpawnSnake")
--- scripts/snake.gd
+++ scripts/snake.gd
@@ -1,9 +1,10 @@
 extends Path2D
 
 var SnakeOrb = load("res://scenes/snake_orb.tscn")
+var GameMaster
 var Body = []
-# @export var BaseSpeed : float # Modify through inspector
-var BaseSpeed = 0.0004
+# @export var BaseSpeed : float # Modify through inspector # does not work, inspector rounds to 0 below 0.001
+var BaseSpeed = 0.0003
 var OrbSpacing = 0.055
 
 func Generate(numbers) -> void:
@@ -21,14 +22,51 @@ func AddOrb(num, pos) -> void:
 		Body.append(NewOrb)
 	else:
 		Body.insert(pos, NewOrb)
+	
 
-func UpdatePosition() -> void:
+func GetLength():
+	return len(Body)
+
+func GetOrb(pos):
+	return Body[pos].GetNumber()
+
+# Currently set to 500% BaseSpeed at start of track and 100% at end with exponential scaling
+# Takes almost exactly 30 seconds to reach the end
+# I think this feels pretty good but I expect it to change when we add orb insertion
+# Mess around with this and find values you think are fun
+func UpdatePosition() -> void: 
+	var ProgressModifier = Body[len(Body)-1].progress_ratio
+	ProgressModifier = 2*(1-ProgressModifier)
+	ProgressModifier = ProgressModifier * ProgressModifier 
 	for orb in Body:
-		orb.UpdateProgress(BaseSpeed)
-		#orb.progress_ratio += BaseSpeed
+		orb.UpdateProgress(BaseSpeed*(1+ProgressModifier))
+
+func SuccessfulCollision(Snorb):
+	var Position = -1
+	for i in range(0, len(Body)):
+		if Body[i] == Snorb:
+			Position = i
+	if Position == -1:
+		print(str(Snorb.GetNumber()))
+	if Snorb.GetNumber() == 1:
+		GameMaster.UpdateScore()
+		DeleteSnorb(Position)
+	else:
+		InsertSnorb(Position)
+
+func DeleteSnorb(Position):
+	var Snorb = Body[Position]
+	Body.remove_at(Position)
+	Snorb.queue_free()
+	if len(Body) == 0:
+		GameMaster.DeleteSnake(self)
+
+func InsertSnorb(Position):
+	pass #TODO
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	GameMaster = get_parent()
 	pass # Replace with function body.
 
 
